36c12f7d54b3bc5e59250fb9871c6520e7ab8025
Julien Joseph Thomas <julien@keyvalue.systems>
2023-06-12 09:14:40 +0000

    Merged in WIP-search-properties-for-event-api (pull request #10)
    WIP replace-metadata-track-api
    
    Approved-by: Vinay Gregory John
    Approved-by: Jiju S Jacob

--- msd_sdk/Presenter/EventPresenter.swift
+++ msd_sdk/Presenter/EventPresenter.swift
@@ -37,15 +37,18 @@ class EventPresenter: BasePresenter {
             self.discoverEvents()
         }
         
-        var finalProperties = [String:Any?]()
-        if let fetchedProperties = properties {
-            finalProperties = fetchedProperties
-        }
-        
+        var finalProperties = properties ?? [:]
         finalProperties.updateValue(eventName, forKey: "event_name")
         finalProperties.updateValue(pageName, forKey: "page_name")
-        addDefaultProperties(properties: &finalProperties)
-        addTrackDefaultProperties(properties: &finalProperties)
+        
+        // Fetch the default properties for the track API
+        var defaultTrackEventProperties = [String:Any?]()
+        addDefaultProperties(properties: &defaultTrackEventProperties)
+        addTrackDefaultProperties(properties: &defaultTrackEventProperties)
+        
+        // Update the keys of the properties map based on the current event name
+        defaultTrackEventProperties = getPropertiesForEventName(eventName, defaultKeys: defaultTrackEventProperties)
+        finalProperties.merge(defaultTrackEventProperties) { (_, new) in new }
         
         await msdservice.track(body: finalProperties, success: { response in
             SDKLogger.shared.logSDKInfo(LOG_INFO_TAG_EVENT_TRACKING, String(describing: response))
@@ -53,11 +56,33 @@ class EventPresenter: BasePresenter {
             SDKLogger.shared.logSDKInfo(LOG_INFO_TAG_EVENT_TRACKING, String(describing: error))
         })
     }
+}
 
+extension EventPresenter{
     func addTrackDefaultProperties(properties: inout [String:Any?]){
         properties.updateValue(ios, forKey: PLATFORM)
         properties.updateValue(application, forKey: MEDIUM)
         properties.updateValue(getCurrentTimestamp(), forKey:TIME_STAMP)
         properties.updateValue(ios, forKey: REFERRER)
+    }
+    
+    func getPropertiesForEventName(_ eventName: String, defaultKeys: [String: Any?]) -> [String: Any] {
+        var properties: [String: Any] = [:]
+        
+        for (defaultKey, value) in defaultKeys {
+            let keyToUpdate = findSourceField(eventName: eventName, metaName: defaultKey) ?? defaultKey
+            if let unwrappedValue = value {
+                properties[keyToUpdate] = unwrappedValue
+            }
+        }
+        return properties
+    }
+    
+    func findSourceField(eventName: String, metaName: String) -> String? {
+        guard let event = discoverReponseList?.data?.events?.first(where: { $0.eventName == eventName }),
+              let schema = event.eventsSchema?.first(where: { $0.meta == metaName }) else {
+            return nil
+        }
+        return schema.sourceField
     }
 }
